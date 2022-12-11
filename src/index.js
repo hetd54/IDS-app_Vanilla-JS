c62ab63e2bae26b88ef6768d05fa727c73fb3d3b
Heather Yu <35639529+hetd54@users.noreply.github.com>
2022-12-11 14:48:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import "./styles.css";
-import $ from "jquery";
-
 /////////////////////////
 /// Helper Functions
 /////////////////////////
@@ -31,9 +28,8 @@ const getCurrentTime = () => {
 const showVideo = (id) => {
   const video = document.getElementById(id);
   // Un-collapse the element
-  video.style.visibility = "visible";
-  // Slow fade in for flavor, then play the video
-  video.animate({ opacity: 1, duration: 1400 }).then(() => video.play());
+  video.style.display = "revert";
+  video.play();
 };
 
 const swapSlide = (idShow, idHide) => {
@@ -68,8 +64,8 @@ function Restart() {
   }
 }
 
-const processOneRow = () => {
-  let fname = experiment.subid + ".csv";
+const processOneRow = () => { // TODO: remove reliance on jquery to post
+  let fname = `${experiment.subjectId}_${experiment.date}.csv`;
   let dataforRound =
     experiment.device +
     "," +
@@ -126,6 +122,7 @@ const processOneRow = () => {
 /////////////////////////
 /// Experiment Setup and Experiment-Specific Functions
 /////////////////////////
+// boolean for allowing clicks
 let videoFinished = false;
 // list of possible reward videos, which we are not counterbalancing.
 let reward = [
@@ -154,7 +151,7 @@ let experiment = {
   // No Inference or Inference condition
   condition: "",
   //trial number
-  trialNumber: 0,
+  trialNumber: 1,
   //type of trial (warmup or main)
   trialType: "",
   //whether child received list 1, 2, 3, or 4
@@ -180,22 +177,24 @@ let experiment = {
 
   warmupTrials: 2,
 
-  maxtrial: 8
+  maxTrial: 8
 };
 
+
 const preStudy = () => {
-  console.log("show consent");
   experiment.subjectId = document.getElementById("subjectID").value;
   experiment.birthday = document.getElementById("birthDate").value;
   experiment.list = document.getElementById("list").value;
   experiment.condition = document.getElementById("condition").value;
   experiment.term = document.getElementById("term").value;
   experiment.sex = document.getElementById("sex").value;
+  experiment.date = `${getCurrentDate()}-${getCurrentTime()}`;
+  console.log(experiment);
 
-  // Load video locations -- two warmup trials are constant between trials
+  // Load video locations -- warmup trials are constant between trials
   const warmupTrials = [
-    "~/videos/Bucket/warmup/warmup-1.mp4",
-    "~/videos/Bucket/warmup/warmup-2.mp4"
+    "./assets/videos/Bucket/warmup/warmup-1.mp4",
+    "./assets/videos/Bucket/warmup/warmup-2.mp4"
   ];
 
   const warmupBox = document.getElementById("warmup");
@@ -204,13 +203,13 @@ const preStudy = () => {
     video.preload = "auto";
     video.autoplay = false;
     video.src = warmupTrials[i];
-    video.onended = allowTap();
-    video.onclick = onTap("warmup");
-    video.setAttribute("id", `warmup${i}`);
+    video.onended = allowTap;
+    video.onclick = onTap;
+    video.setAttribute("id", `warmup${i+1}`);
     if (warmupBox) {
       warmupBox.appendChild(video);
     }
-  }
+  };
 
   // variables to use in creation of video source
   let conditionList;
@@ -230,11 +229,12 @@ const preStudy = () => {
   let colorBlock2 = shuffle(["black", "red", "yellow", "green"]);
   if (colorBlock1[3] === colorBlock2[0]) {
     colorBlock2.push(colorBlock2.shift());
-  }
+  };
   const color = colorBlock1.concat(colorBlock2);
-
-  // There are currently two conditions -- one where the target box is shown to the baby (No-Inference) and one where the empty box is shown (Inference)
-  if (experiment.condition === "No-Inference") {
+  console.log(`${color[0]}`);
+  // There are currently two conditions -- one where the target box is 
+  // shown to the baby (No-Inference) and one where the empty box is shown (Inference)
+  if (experiment.condition === "noinf") {
     conditionList = [
       "noinf-right",
       "noinf-right",
@@ -245,7 +245,7 @@ const preStudy = () => {
       "noinf-right",
       "noinf-right"
     ];
-  } else if (experiment.condition === "Inference") {
+  } else if (experiment.condition === "inf") {
     conditionList = [
       "inf-right",
       "inf-right",
@@ -256,12 +256,12 @@ const preStudy = () => {
       "inf-right",
       "inf-right"
     ];
-  }
+  };
 
   // For counterbalancing of which side is lifted first
   if (
-    (experiment.condition === "No-Inference" && experiment.list === "2") ||
-    (experiment.condition === "No-Inference" && experiment.list === "4")
+    (experiment.condition === "noinf" && experiment.list === "2") ||
+    (experiment.condition === "noinf" && experiment.list === "4")
   ) {
     conditionList = [
       "noinf-left",
@@ -274,8 +274,8 @@ const preStudy = () => {
       "noinf-left"
     ];
   } else if (
-    (experiment.condition === "Inference" && experiment.list === "2") ||
-    (experiment.condition === "Inference" && experiment.list === "4")
+    (experiment.condition === "inf" && experiment.list === "2") ||
+    (experiment.condition === "inf" && experiment.list === "4")
   ) {
     conditionList = [
       "inf-left",
@@ -287,32 +287,32 @@ const preStudy = () => {
       "inf-left",
       "inf-left"
     ];
-  }
+  };
 
   // For counterbalancing of which side contains the ball (target)
   if (experiment.list === "2" || experiment.list === "3") {
     targetSide = targetSide.reverse();
-  }
-
+  };
+  
   /* Use the above information to create a list of videos to pre-load */
   const box = document.getElementById("videoBox");
   for (let i = 0; i < experiment.maxTrial; i++) {
     let video = document.createElement("video");
-    video.setAttribute("id", `test${i}`);
+    video.setAttribute("id", `test${i+1}`);
     video.preload = "auto";
     video.autoplay = false;
-    video.src = `~/videos/Bucket/${targetSide[i]}/${color[i]}/${conditionList[i]}.mp4`;
-    video.onended = allowTap();
-    video.onclick = onTap("main");
-    console.log(video.src);
+    video.src = `./assets/videos/Bucket/${targetSide[i]}/${color[i]}/${conditionList[i]}.mp4`;
+    video.onended = allowTap;
+    video.onclick = onTap;
+
     if (box) {
       box.appendChild(video);
     }
-  }
+  };
 
   //set headers for the CSV File on the server
-  const fname = `${experiment.subjectId}_${getCurrentDate}_${getCurrentTime}.csv`;
-  var dataforRound =
+  const fname = `${experiment.subjectId}_${experiment.date}.csv`;
+  let dataforRound =
     "SubjectID,BirthDate,FullTerm,Sex,List,Condition,TrialNumber,TrialType,BoxColor,SideLiftedFirst,TargetSide,ChosenSide,Response,Date,Time,ReactionTime\n";
   $.post(
     "http://pclpsrescit2.services.brown.edu/blt_lab/ids/data/tabletstudysave.php",
@@ -321,15 +321,17 @@ const preStudy = () => {
   swapSlide("consent", "instructions");
 };
 
+// checking that the parent can hear the test animation audio
 const checkAudio = () => {
   const startWarmupButton = document.getElementById("startWarmup");
   swapSlide("audioCheck", "consent");
   // when the video finishes playing, show the button to continue
   document
     .getElementById("audioVideo")
-    .addEventListener("oneneded", function () {
-      startWarmupButton.display = "block";
-    });
+    .onended = (event) => {
+          startWarmupButton.style.display = "block";
+    };
+    showVideo("audioVideo");
 };
 
 const startWarmup = () => {
@@ -337,16 +339,22 @@ const startWarmup = () => {
   showVideo("warmup1");
 };
 
-const onTap = (e, trialType) => {
+const onTap = (e) => {
   const clickedVideo = document.getElementById(e.target.id);
+  let trialType = ""
+  if (clickedVideo.id.includes("warmup")){
+    trialType = "warmup";
+  } else {
+    trialType = "main";
+  }
   experiment.trialType = trialType;
 
   // Set values for figuring out where the parent clicks
-  var pWidth = $(clickedVideo).innerWidth();
-  var pHeight = $(clickedVideo).innerHeight();
-  var pOffset = $(clickedVideo).offset();
-  var x = e.pageX - pOffset.left;
-  var y = e.pageY - pOffset.top;
+  let pWidth = $(clickedVideo).innerWidth();
+  let pHeight = $(clickedVideo).innerHeight();
+  let pOffset = $(clickedVideo).offset();
+  let x = e.pageX - pOffset.left;
+  let y = e.pageY - pOffset.top;
 
   if ((pWidth / 6 > x) & (pHeight / 5 > y)) {
     Restart();
@@ -386,44 +394,50 @@ const onTap = (e, trialType) => {
           0,
           clickedVideo.src.lastIndexOf("/")
         )}/test-${experiment.trialNumber}.mp4`;
-        clickedVideo
-          .play()
-          .then(() => (clickedVideo.display = "none"))
-          .then(() => {
-            if (experiment.trialNumber === experiment.warmupTrials) {
-              // reached max number of warmup trials
-              experiment.trialNumber = 0;
-              swapSlide("videoBox", "warmup");
-              showVideo(`test${experiment.trialNumber}`);
-            } else {
-              showVideo(`warmup${experiment.trialNumber + 1}`);
-            }
-          });
+        clickedVideo.removeEventListener("ended", allowTap);
+        clickedVideo.onended = () => {
+          clickedVideo.style.display = "none";
+          console.log(experiment.trialNumber);
+          if (experiment.trialNumber  >= experiment.warmupTrials) {
+            // reached max number of warmup trials
+            experiment.trialNumber = 1;
+            swapSlide("videoBox", "warmup");
+            showVideo(`test${experiment.trialNumber}`);
+          } else {
+            showVideo(`warmup${experiment.trialNumber + 1}`);
+          }
+          experiment.trialNumber += 1;
+
+        };
+        clickedVideo.play();
       } else {
         clickedVideo.src = `${clickedVideo.src.substring(
           0,
           clickedVideo.src.lastIndexOf("/")
         )}/${reward[0]}.mp4`;
         reward.splice(0, 1);
-        clickedVideo
-          .play()
-          .then(() => (clickedVideo.visibility = "collapse"))
-          .then(() => {
-            if (experiment.trialNumber === experiment.maxTrial) {
-              // reached max number of trials
-              swapSlide("thanks", "videoBox");
-            } else {
-              showVideo(`test${experiment.trialNumber + 1}`);
-            }
-          });
+        clickedVideo.removeEventListener("ended", allowTap);
+        clickedVideo.onended = () => {
+          clickedVideo.style.display = "none";
+          console.log(experiment.trialNumber);
+          if (experiment.trialNumber  >= experiment.maxTrial) {
+            // reached max number of trials
+            swapSlide("finished", "videoBox");
+          } else {
+            showVideo(`test${experiment.trialNumber}`);
+          }
+          experiment.trialNumber += 1;
+        };
+        clickedVideo.play();
       }
 
       // Send data for this trial to the excel sheet
       processOneRow();
-      experiment.trialNumber += 1;
+      
     } else {
       experiment.response = "N";
-      // Only move on if we are in the test trials
+      // For the main test trials only, we will move on if the 
+      // child picks the incorrect side
       if (trialType === "main") {
         videoFinished = false;
         clickedVideo.src = `${clickedVideo.src.substring(
@@ -432,17 +446,18 @@ const onTap = (e, trialType) => {
         )}/incorrect.mp4`;
         processOneRow();
         experiment.trialNumber += 1;
-        clickedVideo
-          .play()
-          .then(() => (clickedVideo.visibility = "collapse"))
-          .then(() => {
-            if (experiment.trialNumber === experiment.maxTrial) {
-              // reached max number of trials
-              swapSlide("thanks", "videoBox");
-            } else {
-              showVideo(`test${experiment.trialNumber + 1}`);
-            }
-          });
+        clickedVideo.removeEventListener("ended", allowTap);
+        clickedVideo.onended = () => {
+          clickedVideo.style.display = "none";
+          if (experiment.trialNumber  >= experiment.maxTrial) {
+            // reached max number of trials
+            swapSlide("finished", "videoBox");
+          } else {
+            showVideo(`test${experiment.trialNumber}`);
+          }
+          experiment.trialNumber += 1;
+        };
+        clickedVideo.play();
       }
     }
   }
